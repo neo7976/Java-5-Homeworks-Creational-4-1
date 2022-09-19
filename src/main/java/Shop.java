fdcc36e1e9c8f3f134cddea478caaca74d66ff2e
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-19 12:26:07 +0300

    Получили общее меню еды и выход из покупок

---
+++ src/main/java/Shop.java
@@ -0,0 +1,28 @@
+import product.Product;
+import product.ProductFactory;
+import product.ProductType;
+
+public class Shop {
+    public static void main(String[] args) {
+        ProductFactory pf = new ProductFactory();
+        System.out.println("Добро пожаловать в наш магазин!");
+        while (true) {
+            System.out.println("Для совершения покупки нажмите 1 или \"end\" для выхода");
+            String type = pf.scanner.nextLine();
+            if (type.equals("end"))
+                break;
+
+            switch (type) {
+                case "1" -> {
+                    System.out.println("В нашем магазине доступны следующие категории продуктов: ");
+                    pf.getList();
+                    pf.getProduct(Enum.valueOf(ProductType.class, pf.scanner.nextLine()));
+                    pf.getListBasket();
+                }
+                case "2" -> System.out.println("Меню ещё в разработке");
+                default -> System.out.println("Повторите ввод команды");
+            }
+        }
+        //todo Прописать корзину, какие продукты и в каком количестве получили и счёт за них
+    }
+}
--- src/main/java/product/ProductFactory.java
+++ src/main/java/product/ProductFactory.java
@@ -1,27 +1,29 @@
 package product;
 
-import product.Product;
-import product.ProductImp;
-import product.ProductType;
 import product.bread.BreadFactory;
 import product.bread.BreadType;
-import product.bread.Wheat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ProductFactory extends ProductImp {
+    private List<ProductType> productTypeList;
+    private final List<Product> listBasket = new ArrayList<>();
     BreadFactory breadFactory = new BreadFactory();
-    Scanner scanner = new Scanner(System.in);
+    public Scanner scanner = new Scanner(System.in);
 
     public Product getProduct(ProductType productType) {
-
         Product product = null;
 
         switch (productType) {
             case BREAD -> {
                 //получаем все наши значения хлеба и выбираем из списка
+                printProductToBasket("Хлеб");
                 System.out.println(breadFactory.getList());
                 product = breadFactory.getBread(Enum.valueOf(BreadType.class, scanner.nextLine()));
+                listBasket.add(product);
             }
             case FRUIT -> {
             }
@@ -29,12 +31,22 @@ public class ProductFactory extends ProductImp {
         return product;
     }
 
-    public static void main(String[] args) {
-        ProductFactory pf = new ProductFactory();
-        System.out.println(pf.getProduct(Enum.valueOf(ProductType.class, pf.scanner.nextLine())));
+    public void getList() {
+        if (this.productTypeList == null) {
+            productTypeList = new ArrayList<>();
+            productTypeList.addAll(Arrays.asList(ProductType.values()));
+        }
+        System.out.println(productTypeList);
+    }
 
-//        System.out.println(pf.getProduct(ProductType.BREAD).toString());
-//        System.out.println(pf.getProduct(ProductType.BREAD).toString());
-//        System.out.println(pf.getProduct(ProductType.BREAD).toString());
+    public void printProductToBasket(String name) {
+        System.out.printf("Какой \"%s\" добавить в корзину?\n", name);
+    }
+
+    public void getListBasket() {
+        System.out.println("В вашей корзине имеются следующие продукты: ");
+        for (Product product : listBasket) {
+            System.out.println(product);
+        }
     }
 }
