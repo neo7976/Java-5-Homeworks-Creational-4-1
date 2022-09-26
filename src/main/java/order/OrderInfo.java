bfb839d8c22e968a0fd8f063ac254e2f0992bc66
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-26 10:34:01 +0300

    OrderInfoFileRepository.java

--- src/main/java/order/OrderInfo.java
+++ src/main/java/order/OrderInfo.java
@@ -6,8 +6,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 //Сделать как класс записи в отдельный файл, чтобы можно было прочитать данные
-public class Order {
+public class OrderInfo {
     private String id;
+    private Map<Product, Integer> mapOrder;
     private String name;
     private int price;
     private int weight;
@@ -15,7 +16,19 @@ public class Order {
     private int countSum;
     private int countTotal;
 
-    private Map<Product, Integer> mapOrder;
+
+
+
+    public OrderInfo(String id, Map<Product, Integer> mapOrder, int countSum, int countTotal) {
+        this.id = id;
+        this.mapOrder = mapOrder;
+        this.countSum = countSum;
+        this.countTotal = countTotal;
+    }
+
+    public OrderInfo( Map<Product, Integer> mapOrder, int countSum, int countTotal) {
+        this(null, mapOrder, countSum, countTotal);
+    }
 
     public void setMapOrder(Map<Product, Integer> mapOrder) {
         this.mapOrder = mapOrder;
@@ -38,5 +51,29 @@ public class Order {
 
         //на этом шаге лучше сделать запись в файл
         mapOrder = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getCountSum() {
+        return countSum;
+    }
+
+    public int getCountTotal() {
+        return countTotal;
     }
 }
